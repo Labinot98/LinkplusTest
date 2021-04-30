524c0ca89fb4971f076fe8bc18f64cefd797e245
Labinot Pajaziti <38728474+Labinot98@users.noreply.github.com>
2021-04-30 14:58:07 +0200

    Create detail view and create one funcition for generate flag

--- Linkplus/Controller/DetailVC.swift
+++ Linkplus/Controller/DetailVC.swift
@@ -13,8 +13,20 @@ class DetailVC: UIViewController {
     @IBOutlet weak var LastName: UILabel!
     @IBOutlet weak var emailUser: UILabel!
     
+    var product = Results()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        FirstName.text = product.userName?.first
+        LastName.text = product.userName?.last
+        emailUser.text = product.userEmail
+        
+        let image_name = product.userPicture?.large
+        let image_url = ( image_name!).addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!
+        if let url = URL(string: image_url) {
+            imageUser.af_setImage(withURL: url)
+        }
 
         // Do any additional setup after loading the view.
     }
--- Linkplus/Controller/HomeVC.swift
+++ Linkplus/Controller/HomeVC.swift
@@ -55,13 +55,30 @@ class HomeVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
         cell.firstName.text = product.userName?.first
         cell.lastName.text = product.userName?.last
         cell.AgeUser.text = String(x)
-        cell.nationality.text = product.userNat
-        
+//        cell.nationality.text = product.userNat
+        cell.nationality.text = flag(country: product.userNat!)
         return cell
         
     }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let product = arrayOfUsers[indexPath.row]
+        let vc = self.storyboard?.instantiateViewController(withIdentifier: "DetailVC") as! DetailVC
+        vc.product = product
+        let nav = UINavigationController(rootViewController: vc)
+//        self.present(nav, animated: true)
+        self.navigationController?.pushViewController(vc, animated: true)
+    }
 
-
+    // MARK: - Convert data notionality from api in flag icon
+    func flag(country:String) -> String {
+        let base : UInt32 = 127397
+        var s = ""
+        for v in country.unicodeScalars {
+            s.unicodeScalars.append(UnicodeScalar(base + v.value)!)
+        }
+        return String(s)
+    }
 }
 
 extension String {
